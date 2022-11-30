367ea139ac61eaf1a88d6ac024eec778c24d06b5
AlexSMiheev <aleksandr_miheev@bitnet.by>
2022-11-30 11:17:35 +0300

    fix for the long calculation formula

--- homework2/task_one.py
+++ homework2/task_one.py
@@ -9,6 +9,6 @@ try:
     print(f'Если координаты первой точки {x_first} и {y_first}, '
           f'а координаты для второй {x_second} и {y_second}, '
           f'то расстояние между ними составит - '
-          f'{sqrt(int(x_second) - int(x_first)) ** 2 + (int(y_second) - int(y_first)) ** 2}')
+          f'{sqrt(pow(int(x_second) - int(x_first), 2) + pow(int(y_second) - int(y_first), 2))}')
 except ValueError:
     print('Невозможно рассчитать в этом измерении.')
