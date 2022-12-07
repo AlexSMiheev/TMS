2ae068dd32f6a9e4676e146ed1f9d5cee754a219
AlexSMiheev <aleksandr_miheev@bitnet.by>
2022-12-07 13:17:23 +0300

    fix for hw5

--- homework5/task_one.py
+++ homework5/task_one.py
@@ -3,8 +3,9 @@ phones_db = {'25': {'None': 'Life:)'},
                     '6': 'A1', '7': 'MTC', '8': 'MTC', '9': 'A1'},
              '33': {'None': 'MTC'},
              '44': {'None': 'A1'}}
+next_number = True
 
-while True:
+while next_number:
     result = {}
     success = None
     user_phone = input('Enter phone number: ')
@@ -20,14 +21,18 @@ while True:
                     success = True
                     for key in phones_db:
                         if key == user_phone[4:6]:
-                            result['success'] = success
+                            result['success'] = True
                             result['phone'] = user_phone
                             for n in phones_db[key]:
                                 if user_phone[6] == n:
+                                    result['success'] = True
                                     result['operator'] = phones_db[key][n]
                                 elif n == 'None':
+                                    result['success'] = True
                                     result['operator'] = phones_db[key]['None']
-                        else:
+                        try:
+                            result['success']
+                        except KeyError:
                             result['success'] = False
                             result['description'] = 'Operator code check not passed'
                 else:
@@ -44,3 +49,7 @@ while True:
         result['description'] = 'Len check not passed'
 
     print(result)
+    if input('Would you like to check out another one? (Y / N): ').lower() == 'y':
+        continue
+    else:
+        next_number = False
--- homework5/task_third.py
+++ homework5/task_third.py
@@ -4,7 +4,7 @@ text = open('text.txt')
 quantity = 0
 for i in text:
     for c in i:
-        if c == char:
+        if c.lower() == char.lower():
             quantity += 1
 
 print(f'Result: the letter occurs {quantity} times in the text.')
